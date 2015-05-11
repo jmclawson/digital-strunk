7544d046126c961eda3c80804f29ffdae57a4696
jmclawson <jmclawson@users.noreply.github.com>
2015-05-11 19:54:00 -0400

    convert to jquery

--- divhiding.js
+++ divhiding.js
@@ -16,3 +16,13 @@ function showonlyonev2(thechosenone) {
             }
       }
 }
+function showonlyone(thechosenone) {
+     $('.newboxes').each(function(index) {
+          if ($(this).attr("id") == thechosenone) {
+               $(this).show(200);
+          }
+          else {
+               $(this).hide(600);
+          }
+     });
+}
