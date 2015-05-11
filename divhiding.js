ab7a58458fae40ad9f4a69d6a37b088b30c65d2d
jmclawson <jmclawson@users.noreply.github.com>
2015-05-11 18:42:47 -0400

    Create divhiding.js

---
+++ divhiding.js
@@ -0,0 +1,18 @@
+function showonlyonev2(thechosenone) {
+      var newboxes = document.getElementsByTagName("div");
+      for(var x=0; x<newboxes.length; x++) {
+            name = newboxes[x].getAttribute("class");
+            if (name == 'newboxes-2') {
+                  if (newboxes[x].id == thechosenone) {
+                        if (newboxes[x].style.display == 'block') {
+                              newboxes[x].style.display = 'none';
+                        }
+                        else {
+                              newboxes[x].style.display = 'block';
+                        }
+                  }else {
+                        newboxes[x].style.display = 'none';
+                  }
+            }
+      }
+}
