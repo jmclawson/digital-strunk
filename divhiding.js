e5ba395058c12e6df125261edc172c0f60c1abbf
jmclawson <jmclawson@users.noreply.github.com>
2015-10-13 09:16:33 -0500

    Temporarily disabling

--- divhiding.js
+++ divhiding.js
@@ -1,16 +1,16 @@
-function showonlyone(thechosenone) {
-     $('.newboxes').each(function(index) {
-          if ($(this).attr("id") == thechosenone) {
-               if ($(this).is(':visible')) {
-                     $(this).hide(600);
-               }
-               else {
-                     $(this).show(200);
-               }
-          }
-          else {
-               $(this).hide(600);
-          }
-     });
-}
+function showonlyone(thechosenone) {}
+//     $('.newboxes').each(function(index) {
+//          if ($(this).attr("id") == thechosenone) {
+//               if ($(this).is(':visible')) {
+//                     $(this).hide(600);
+//               }
+//               else {
+//                     $(this).show(200);
+//               }
+//          }
+//          else {
+//               $(this).hide(600);
+//          }
+//     });
+//}
 // adapted from http://www.randomsnippets.com/2011/04/10/how-to-hide-show-or-toggle-your-div-with-jquery/
